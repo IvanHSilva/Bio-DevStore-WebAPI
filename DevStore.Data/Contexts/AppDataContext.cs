a24f98bf4e0fe91b218a5d396fe57100d01f611d
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-11-25 20:50:49 -0300

    Product Repository + DataContext

---
+++ DevStore.Data/Contexts/AppDataContext.cs
@@ -0,0 +1,10 @@
+﻿using DevAccounts.Domain;
+using System.Data.Entity;
+
+namespace DevStore.Data.Contexts {
+    public class AppDataContext : DbContext {
+        public AppDataContext():base("AppConStr"){}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ DevStore.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,45 @@
+﻿using DevAccounts.Domain;
+using DevAccounts.Domain.Repositories;
+using DevStore.Data.Contexts;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DevStore.Data.Repositories {
+    public class ProductRepository : IProductRepository {
+
+        private readonly AppDataContext _db;
+
+        public ProductRepository() {
+            _db = new AppDataContext();
+        }
+
+        public List<Product> GetProducts() {
+            return _db.Products.ToList();
+        }
+
+        public Product GetProduct(int id) {
+            return _db.Products.Find(id);
+        }
+
+        public void InsertProduct(Product product) {
+            _db.Products.Add(product);
+            _db.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product) {
+            _db.Entry<Product>(product).State = EntityState.Modified;
+            _db.SaveChanges();
+
+        }
+
+        public void DeleteProduct(int id) {
+            _db.Products.Remove(GetProduct(id));
+            _db.SaveChanges();
+        }
+
+        public void Dispose() {
+            _db.Dispose();
+        }
+    }
+}

163bc9172ceaa36a705c805a6ec1dd8c05439552
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-11-26 13:44:31 -0300

    ProductsApiController

---
+++ DevStore.Web/Controllers/ProductsApiController.cs
@@ -0,0 +1,24 @@
+﻿using DevAccounts.Domain;
+using DevAccounts.Domain.Repositories;
+using DevStore.Data.Repositories;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DevStore.Web.Controllers {
+    public class ProductsApiController : ApiController {
+
+        private readonly IProductRepository _repository;
+
+        public ProductsApiController() {
+            _repository = new ProductRepository();
+        }
+
+        public List<Product> GetProducts() {
+            return _repository.GetProducts();
+        }
+
+        protected override void Dispose(bool disposing) {
+            _repository.Dispose();
+        }
+    }
+}
